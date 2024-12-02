538fe64a02c11fc49f8cc123a56af1d11da9f79b
EthanSpearey <1302228@spsd.sk.ca>
2024-12-02 12:14:53 -0600

    First Day of Actual Code
    I have final started to add some code for movement as well as preloading some of the assets in the game.

--- Metal Gear Solid VR Missions/sketch.js
+++ Metal Gear Solid VR Missions/sketch.js
@@ -2,6 +2,24 @@
 // Ethan Spearey
 // 2024-11-21
 
+let sprites;
+let playerX, playerY;
+
+function preload()
+{
+  for (let i = 1; i < 8; i++)
+  {
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run North" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run North-East" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run East" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run South-East" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run South" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run South-West" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run West" + i + ".png"));
+    sprites.push(loadImage("assets/New Sprite Frames/Solid Snake Run North-West" + i + ".png"));
+  }
+}
+
 function setup()
 {
   createCanvas(160, 144);
@@ -9,5 +27,25 @@ function setup()
 
 function draw()
 {
-  background(220);
+  background(255);
+}
+
+function keyPressed()
+{
+  if(keyCode === 87) //W Key
+  {
+    playerY++;
+  }
+  if(keyCode === 65) //A Key
+  {
+    playerX--;
+  }
+  if(keyCode === 83) //S Key
+  {
+    playerY--;
+  }
+  if(keyCode === 68) //D Key
+  {
+    playerX++;
+  }
 }
